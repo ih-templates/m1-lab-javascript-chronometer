217f73ec6f26bc928c72f2232e2847aa01057d65
Juan Grajales <grajales.juanfe11@gmail.com>
2020-04-08 13:00:47 -0400

    complete-lab-with-notes

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,79 @@
+// Iteration 1 - complete the Chronometer class
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentSeconds = 0;
+    this.currentMilliSec = 0;  // BONUS
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    // setInterval - https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+    // setInterval Examples - https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval#Examples
+    // setInterval(function that will execute every delay, delay in milliseconds)
+    this.intervalId = setInterval(() => {
+      this.currentSeconds++;    
+      // callback function will be printing the time
+      if (callback) 
+        callback();
+    }, 1000);
   }
+
+  // BONUS with milliseconds
+  // startClick(callback) {
+  //   this.intervalId = setInterval(() => {
+  //     this.currentMilliSec++;
+  //     if(this.currentMilliSec % 100 === 0){
+  //       this.currentSeconds++;    
+  //     }
+  //     // callback function will be printing the time
+  //     if (callback) 
+  //       callback();
+  //   }, 10);
+  // }
+
   getMinutes() {
-    // ... your code goes here
+    // logic 
+    // 1sec/ 60 = decimal less than 1 round down to 0 
+    // 390sec/ 60 = 3.5 round down to 3
+    return Math.floor(this.currentSeconds / 60);
   }
+
   getSeconds() {
-    // ... your code goes here
+    // this will give you the remaining milliseconds
+    return this.currentSeconds % 60;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  // BONUS
+  getMilliseconds() {
+    return this.currentMilliSec % 100;
   }
+
+  twoDigitsNumber(value) {
+    if (value < 10) 
+      return `0${value}`;
+    else
+      return `${value}`;
+  }
+
   stopClick() {
-    // ... your code goes here
+    // clearInterval - https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentSeconds = 0;
   }
+
+  // split for min:sec
+  // splitClick() {
+  //   return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
+  // }
+
+  // BONUS
+  // split for min:sec:millisec
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(
+      this.getSeconds()
+    )}:${this.twoDigitsNumber(this.getMilliseconds())}`;
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -14,52 +14,104 @@ let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printMinutes();
+  printSeconds();
+  printMilliseconds();  // BONUS
 }
 
+// change the innerHTML of the minutes 
 function printMinutes() {
-  // ... your code goes here
+  // twoDigitsNumber() returns a 2 character string
+  minUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[1];
+  minDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[0];
 }
 
+// change the innerHTML of the seconds
 function printSeconds() {
-  // ... your code goes here
+  // twoDigitsNumber() returns a 2 character string
+  secUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[1];
+  secDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[0];
 }
 
-// ==> BONUS
+// BONUS
 function printMilliseconds() {
-  // ... your code goes here
+  milUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getMilliseconds())[1];
+  milDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMilliseconds())[0];
 }
 
 function printSplit() {
-  // ... your code goes here
+  // create an <li> element
+  let li = document.createElement('li');
+  // add the split content to the <li> element
+  li.innerHTML = chronometer.splitClick();
+  // add the <li> element to the <ol> element
+  // appendChild - https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+  splits.appendChild(li);
 }
 
 function clearSplits() {
-  // ... your code goes here
+  // remove all <li> elements from the <ol> element
+  splits.innerHTML = '';
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  // set the class='btn stop'
+  btnLeft.className = 'btn stop';
+  // change the text of the button to STOP
+  btnLeft.innerText = 'STOP';
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  // set the class='btn split'
+  btnRight.className = 'btn split';
+  // change the text of the button to SPLIT
+  btnRight.innerHTML = 'SPLIT';
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  // set the class='btn start'
+  btnLeft.className = 'btn start';
+  // change the text of the button to START
+  btnLeft.textContent = 'START';
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  // set the class='btn reset'
+  btnRight.className = 'btn reset';
+  // change the text of the button to RESET
+  btnRight.textContent = 'RESET';
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeft.classList.contains('start')) {
+    // start the timer and print the time after every second
+    chronometer.startClick(printTime);
+    // change the start button to stop
+    setStopBtn();
+    // change the reset button to split
+    setSplitBtn();
+  } else {
+    // stop the timer 
+    chronometer.stopClick();
+    // change the stop button to start
+    setStartBtn();
+    // change the split button to reset
+    setResetBtn();
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnRight.classList.contains('reset')) {
+    // resert the this.currentTime = 0
+    chronometer.resetClick();
+    // remove all the <li> elements from the <ol>
+    clearSplits();
+    // print the time, time should now be 00:00
+    printTime();
+  } else {
+    // add <li> element with the time split to the <ol> element 
+    printSplit();
+  }
 });
